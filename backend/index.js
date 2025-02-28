0e8f0392dd03f411a5d6ab333e88f4aff5bd12e6
Upendra kumar <89980759+upendra14@users.noreply.github.com>
2025-02-28 14:44:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+// const corsOptions = {
+//     origin:'http://localhost:5173',
+//     credentials:true
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
